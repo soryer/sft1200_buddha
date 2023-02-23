360661ddfc2070a7d734c2acfda80683b80b5b7a
Kris <soryer@live.com>
2023-02-23 21:05:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed -i '/sft1200_buddha/d' /etc/opkg/customfeeds.conf
-echo "src/gz sft1200_buddha https://cdn.jsdelivr.net/gh/ericwang2006/sft1200_buddha" >>/etc/opkg/customfeeds.conf
+echo "src/gz sft1200_buddha https://cdn.jsdelivr.net/gh/soryer/sft1200_buddha" >>/etc/opkg/customfeeds.conf
 opkg update
 action="$1"
 case "${action}" in
@@ -14,5 +14,5 @@ passwall)
 	opkg install luci luci-i18n-base-zh-cn luci-app-passwall luci-i18n-passwall-zh-cn luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn
 	;;
 esac
-wget -O /tmp/libsodium.ipk https://cdn.jsdelivr.net/gh/ericwang2006/sft1200_buddha/libsodium_1.0.18-2021-09-17-64129657_mips_siflower.ipk
+wget -O /tmp/libsodium.ipk https://cdn.jsdelivr.net/gh/soryer/sft1200_buddha/libsodium_1.0.18-2021-09-17-64129657_mips_siflower.ipk
 opkg install /tmp/libsodium.ipk
